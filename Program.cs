717465fc4a926caf6aa160a97120beedb9d81847
Deep in misery <131404446+Betus971@users.noreply.github.com>
2025-11-06 09:42:53 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetFichier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+
+    }
+}
